245a774a65e6824378c94252b94e9a9588945115
The kebab manning <itzsopzzz@gmail.com>
2025-02-09 15:22:58 +0200

    now removes chams when ragdoll

--- AdrenalineCHAMS.lua
+++ AdrenalineCHAMS.lua
@@ -3,6 +3,7 @@
   ]]--
 
 local plrgrp = game.Workspace:FindFirstChild("Players")
+
 if not plrgrp then
     return
 end
@@ -11,8 +12,8 @@ local function addHgh(character)
     if character:FindFirstChildOfClass("Highlight") then return end
     
     local highlight = Instance.new("Highlight")
-    highlight.OutlineTransparency = 1 -- / Removed Outlines cuz of peformance
-    highlight.FillColor = Color3.fromRGB(128, 0, 128) -- / Change color here
+    highlight.OutlineTransparency = 1
+    highlight.FillColor = Color3.fromRGB(128, 0, 128)
     
     highlight.Parent = character
 end
@@ -26,5 +27,14 @@ end
 plrgrp.ChildAdded:Connect(function(character)
     if character:IsA("Model") then
         addHgh(character)
+    end
+end)
+
+game.Workspace.Ragdolls.ChildAdded:Connect(function(model)
+    if model:IsA("Model") then
+        local highlight = model:FindFirstChildOfClass("Highlight")
+        if highlight then
+            highlight:Destroy()
+        end
     end
 end)
